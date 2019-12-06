a45ac2d7b4f4ccb27a453d340239c0e60b8707d8
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2019-12-06 23:57:08 +0200

    git_commands.txt renamed to git_commands.notes - part 2

--- git_commands.c
+++
@@ -1,9 +0,0 @@
-/* GIT TUTORIAL */
-
-git add //working area -> staging area
-git commit //staging area -> repository
-git checkout //repository -> working area
-
-git rm -f <filename> //remove file from working area and index
-git rm --cashed <filename> //remove filename from working are
-git mv <filename> <newFilenameAfterRename> //rename file
