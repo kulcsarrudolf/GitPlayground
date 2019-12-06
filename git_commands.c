5ecb94cb4832e82e98b061e31005a9481ee3c36f
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2019-12-06 23:43:23 +0200

    git_commands.txt renamed to git_commands.c

--- git_commands.c
+++ git_commands.c
@@ -1,3 +1,9 @@
+/* GIT TUTORIAL */
+
+git add //working area -> staging area
+git commit //staging area -> repository
+git checkout //repository -> working area
+
 git rm -f <filename> //remove file from working area and index
 git rm --cashed <filename> //remove filename from working are
 git mv <filename> <newFilenameAfterRename> //rename file
